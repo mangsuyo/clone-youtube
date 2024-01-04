320b656dd47759bdf4a8afcca49b8cf6ed96b2ee
Han <mayo3610@naver.com>
2024-01-04 13:57:04 +0900

    Feat : SearchBar 기능 구현

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+
+export default function SearchBar({ handleSetKeyWord }) {
+  const [text, setText] = useState("");
+  const handleSearchInputChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleSearchContentSubmit = (event) => {
+    event.preventDefault();
+    handleSetKeyWord(text);
+  };
+  return (
+    <header className="px-[10%] py-[1%]">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <img src="images/youtube-logo.avif" className="w-12 h-12 mr-2" />
+          <h1 className="text-xl font-medium">Youtube</h1>
+        </div>
+        <form
+          className="w-[70%] flex items-center border-2"
+          onSubmit={handleSearchContentSubmit}
+        >
+          <input
+            className="w-[90%] pl-5 py-2 outline-none"
+            type="text"
+            placeholder="Search..."
+            value={text}
+            onChange={handleSearchInputChange}
+          />
+          <button className="w-[10%]">
+            <CiSearch className="text-2xl" />
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
 
 export default function Root() {
+  const [keyword, setKeyword] = useState("");
+
+  const handleSetKeyWord = (input) => {
+    setKeyword(input);
+  };
+
   return (
-    <div className="">
-      <Outlet />
-    </div>
+    <article className="w-screen min-h-screen">
+      <SearchBar handleSetKeyWord={handleSetKeyWord} />
+      <article className="px-[10%]">
+        <Outlet />
+      </article>
+    </article>
   );
 }
